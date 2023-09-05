9120ec89bc11ba32f3f27916326ecd8db834f27d
YanYuhtman <yan.yuhtman@gmail.com>
2023-09-05 23:08:51 +0300

    Is the King in check - not complete solution

--- Is_the_King_in_check.py
+++ Is_the_King_in_check.py
@@ -1,12 +1,56 @@
 import unittest
+import itertools as I
+import functools as F
+import numpy as N
+import operator as O
 
 
-is_check = lambda board: True
+# is_check = lambda board: True
+def is_check(board):
+    # tmp = None
+    # mL = list()
+    # for y,s in enumerate(board):
+    #    for x,s in enumerate(s):
+    #         mL += [[y,x,s]]
+    #
+    # tmp = (list(map(lambda y : list(map(lambda x: [y[0],x[0],x[1]],enumerate(y[1]))), enumerate(board))))
+    # tmp = list(map(lambda y: list(map(lambda x: [y[0],x[0],x[1]],enumerate(y[1]))), enumerate(board)))
+
+    tmp = (filter(lambda v:v[2]!=' ',F.reduce(lambda a,b:a+b,[(list(map(lambda X:(Y[0],X[0],X[1]),enumerate(Y[1])))) for Y in enumerate(board)])))
+    tmp = sorted(tmp,key=lambda x:x[2])
+    tmp = list(map(lambda t:(t[0]-tmp[0][0],t[1]-tmp[0][1],t[2]),tmp))
+    tmp = list(map(lambda t:(abs(int(t[0]/t[1]) if t[1] else 0),t[0]**2+t[1]**2,t[2]),tmp))
+    # tmp = F.reduce(lambda a,b: l + b,tmp)
+    tmp = N.unique(sorted(tmp,key=lambda x:x[1]),axis=1)
+    tmp = list(map(lambda t:(
+         {'♞':t[1]=='5',
+         '♝':t[0]=='1',
+         '♟':t[1]=='2',
+         '♜':t[0]=='0',
+         '♛':(t[1]=='1' or t[0]=='0')
+         }
+        .get(t[2])),tmp))
+    return any(tmp)
+    # tmp = [((list(Y[1])) for X in enumerate(Y[1])) for Y in enumerate(board)]
+
+
+    tmp = list(map(lambda by:
+               list(map(lambda bx:(bx[1],by[0],bx[0]),enumerate(by[1]))),enumerate(board)))
+    True
 class MyTestCase(unittest.TestCase):
     is_check = lambda board: False
 
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+    def test_noTest(self):
+        is_check([[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                  [' ', ' ', ' ', '♟', ' ', ' ', ' ', ' '],
+                  [' ', ' ', '♔', ' ', ' ', ' ', ' ', ' '],
+                  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']])
+        self.assertEqual(True,True)
+
     def test_PawnCheck(self):
         self.assertEqual(is_check([[' ',' ',' ',' ',' ',' ',' ',' '],
                                    [' ',' ',' ',' ',' ',' ',' ',' '],
