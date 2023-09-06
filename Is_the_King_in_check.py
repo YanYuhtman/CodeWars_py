e6dcf09202e3cbf7c0445b82519ceb9919746dd1
YanYuhtman <yan.yuhtman@gmail.com>
2023-09-06 17:27:23 +0300

    Is the King in check - reduced some

--- Is_the_King_in_check.py
+++ Is_the_King_in_check.py
@@ -5,7 +5,7 @@ import numpy as N
 import operator as O
 
 
-# is_check=lambda b:any(map(lambda t:({'♞':t[1]==5,'♝':t[0]==1,'♟':t[1]==2,'♜':t[0]==0,'♛':(t[1]==1 or t[0]==0)}[t[2]]),dict(map(lambda t:(t[0],t),sorted(dropwhile(lambda t:t[2]=='♔',map(lambda t:(abs(t[0]/t[1] if t[1] else 0),t[0]**2+t[1]**2,t[2]),(lambda b:map(lambda t:(t[0]-b[0][0],t[1]-b[0][1],t[2]),b))(sorted([[x,y,b[x][y]]for x in range(0,8)for y in range(0,8)if b[x][y]!=' '],key=lambda x:x[2])))),key=lambda t:t[1],reverse=True))).values()))
+is_check=lambda b:any(map(lambda t:({'♞':t[2]==5,'♝':t[1]==1,'♟':t[2]==2,'♜':t[1]==0,'♛':(t[2]==1or t[1]==0)}[t[0]]),dict(map(lambda t:(t[1],t),sorted([[t[0],abs(not t[2]or t[1]/t[2]),t[1]**2+t[2]**2] for t in (lambda b:map(lambda t:(t[0],t[1]-b[0][1],t[2]-b[0][2]),b))(sorted([[b[x][y],x,y]for x in range(0,8)for y in range(0,8)if b[x][y]!=' '])) if t[0]!='♔']))).values()))
 
 def is_check(b):
 
@@ -13,9 +13,7 @@ def is_check(b):
     tmp = [[b[x][y],x,y]for x in range(0,8)for y in range(0,8)if b[x][y]!=' ']
     tmp = sorted(tmp)
     tmp = (lambda b:map(lambda t:(t[0],t[1]-b[0][1],t[2]-b[0][2]),b))(tmp)
-    tmp = map(lambda t:(t[0],abs(not t[2]or t[1]/t[2]),t[1]**2+t[2]**2),tmp)
-    # tmp = list(tmp).pop()
-    tmp = dropwhile(lambda t:t[0]=='♔',tmp)
+    tmp = [[t[0],abs(not t[2]or t[1]/t[2]),t[1]**2+t[2]**2] for t in tmp if t[0]!='♔']
     tmp = dict(map(lambda t:(t[1],t),sorted(tmp)))
     tmp = map(lambda t:(
          {'♞':t[2]==5,
